dc916c7a77ace6b5c66a9d39259a987692a67cfd
Rakib Mahmud <rakibas375@gmail.com>
2023-09-04 08:40:40 +0600

    dependency injection 1st step

--- index.php
+++ index.php
@@ -32,3 +32,36 @@ $std->search("World");
 $std->replace('Rakib');
 $std->print();
 
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+/* Dependency injection 1st step */
+interface BaseStudent{
+    public function displayName(); //no body this function 
+}
+class Student implements BaseStudent{
+    private $name;
+    public function __construct($name)
+    {
+        $this->name=$name;
+    }
+    public function displayName()
+    {
+        echo "Hello ".$this->name;
+    }
+}
+
+class StudentManager {
+    public function introduceStudent($name){
+        $student=new Student($name);
+        $student->displayName();
+    }
+}
+
+$student=new StudentManager();
+$student-> introduceStudent("Rakib Mahmud");
+
+
+
